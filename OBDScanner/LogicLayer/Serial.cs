202df3e0f5c8d158c5f1666f915e8b95dd38893f
Roger1121 <leszczynski_szymon@wp.pl>
2021-02-14 14:24:15 +0100

    Implemented some of the serial methods

--- OBDScanner/LogicLayer/Serial.cs
+++ OBDScanner/LogicLayer/Serial.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.IO.Ports;
 
 namespace LogicLayer
 {
-    class SerialInit
+    class Serial
     {
+        //programm will work with one OBD2 interface at once
+        SerialPort port;
+        public void initSerial(string name = "OBD", int baud = 38400,
+                               Parity parity = Parity.None, int dataBits = 8,
+                               StopBits stopBits = StopBits.One)
+        {
+            port = new SerialPort(name, baud, parity, dataBits, stopBits);
+        }
+        public string parseSerialResponse(string response)
+        {
+            return response;
+        }
+        public string readSerial()
+        {
+            string response=null;
+            return parseSerialResponse(response);
+        }
+        public void writeSerial(string command)
+        {
+
+        }
     }
 }
